bc44c0325ccca420e02a2bc616f6781203acb313
Jose Armando Salazar Moreno <samja1701ap@gmail.com>
2025-08-04 01:24:56 -0600

    jwt

--- src/main/java/com/crud/springboot/app/springboot_crud/repositories/UsersRepository.java
+++ src/main/java/com/crud/springboot/app/springboot_crud/repositories/UsersRepository.java
@@ -1,5 +1,7 @@
 package com.crud.springboot.app.springboot_crud.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,7 @@ import com.crud.springboot.app.springboot_crud.entities.User;
 public interface UsersRepository extends JpaRepository<User, String> {
 
     boolean existsByUsername(String username);
+
+    Optional<User> findByUser(String username);
 
 }
---
+++ src/main/java/com/crud/springboot/app/springboot_crud/services/JpaUserDetailsService.java
@@ -0,0 +1,46 @@
+package com.crud.springboot.app.springboot_crud.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.crud.springboot.app.springboot_crud.entities.User;
+import com.crud.springboot.app.springboot_crud.repositories.UsersRepository;
+
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+
+    private final UsersRepository usersRepository;
+
+    public JpaUserDetailsService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userdb = usersRepository.findByUser(username);
+
+        if (userdb.isEmpty()) {
+            throw new UsernameNotFoundException(String.format("Username %s no existe", username));
+        }
+
+        User user = userdb.orElseThrow();
+
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                user.isEnabled(), true, true, true, authorities);
+
+    }
+
+}
